4da415f59d883ab833d1a1789c30689b11739f87
Nguyễn Thanh Trường <honguyententhanhtruong@gmail.com>
2026-03-23 13:54:28 +0700

    Update Deploy

--- src/main/java/com/example/Server/config/SecurityConfiguration.java
+++ src/main/java/com/example/Server/config/SecurityConfiguration.java
@@ -203,10 +203,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of(
-                "http://localhost:5173",
-                "https://*.vercel.app"
-        ));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
